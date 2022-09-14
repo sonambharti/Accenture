b11e8a307b60b81589d4cdde1322bf833f3fac0f
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2022-09-14 16:24:57 +0530

    SQL Query to create Table

---
+++ RDBMS/Data Definition Language/CreateDistributorTable/createDistributorTable.sql
@@ -0,0 +1,9 @@
+create table distributor(
+	distributor_id varchar2(10),
+	distributor_name varchar2(20),
+	address varchar(100),
+	mobilenumber number(10),
+	email varchar2(30),
+	primary key(distributor_id)
+	);
+	

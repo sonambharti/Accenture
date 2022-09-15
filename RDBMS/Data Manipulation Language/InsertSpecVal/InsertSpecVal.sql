c420cb0e417ce7b6a46e57f5b993ee6d30436e2a
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2022-09-16 01:52:38 +0530

    SQL query to insert Records

---
+++ RDBMS/Data Manipulation Language/InsertSpecVal/InsertSpecVal.sql
@@ -0,0 +1,5 @@
+insert into customer_info(customer_id, customer_name, address, email) values('C01', 'Mary', 'Delhi', 'mary@gmail.com');
+insert into customer_info(customer_id, customer_name, address, email) values('C02', 'Hema', 'Banglore', 'hema@yahoo.com');
+insert into customer_info(customer_id, customer_name, address, email) values('C03', 'Rocky', 'Chennai', 'rocky12@gmail.com');
+insert into customer_info(customer_id, customer_name, address, email) values('C04', 'Robin', 'Mumbai', 'robbywood@yahoo.com');
+insert into customer_info(customer_id, customer_name, address, email) values('C05', 'Swetha', 'Hyderabad', 'Swethu1993@gmail.com');

3d9e4bbc6eeb0a7002281be1bfc6f88b3d8549c7
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2022-09-16 01:47:52 +0530

    Sql query to insert records

---
+++ RDBMS/InsertRecGuest/InsertRecGuest.sql
@@ -0,0 +1,13 @@
+--  Insert Records into Guest table
+-- Insert the following records into "Guest" table based on schema given below
+
+inser into(guestid, name, address, country, email, phone) values
+	('1234', 'RAJ GUPTA', '116 LAKE ROAD OOTACAMUND', 'INDIA', 'RAJ@HOTMAIL.COM', '919090909090');
+
+
+inser into(guestid, name, address, country, email, phone) values
+	('1235', 'JACK MARTIN', 'NO:5ST ANTONYS CHURCH CHENNAI', 'INDIA', 'JACK.1909@GMAIL.COM', '917867867834');
+
+
+inser into(guestid, name, address, country, email, phone) values
+	('1236', 'MARY GREY', '14/2 EAST GHAT ROAD KODAIKANAL', 'INDIA', 'MARK.KOM@YAHOO.COM', '919090909090');

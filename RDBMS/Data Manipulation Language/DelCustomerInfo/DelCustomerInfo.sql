47be0cf712022006ee2933fcff0c1910acff155d
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2022-09-16 02:10:21 +0530

    SQL Query to del/remove info

---
+++ RDBMS/Data Manipulation Language/DelCustomerInfo/DelCustomerInfo.sql
@@ -0,0 +1,5 @@
+--Delete customer information details
+
+--Write a query to remove customer information details of the customer whose name is 'Manish' .
+
+delete from customer_info where customer_name = 'Manish';
---
+++ RDBMS/Data Manipulation Language/RemoveSalesInfo/RemoveSalesInfo.sql
@@ -0,0 +1,5 @@
+--Remove sales information details
+
+--Write a query to remove sales information details of all the mobiles, whose price is 25000.
+
+delete from Sales_Info where Price = 25000;
